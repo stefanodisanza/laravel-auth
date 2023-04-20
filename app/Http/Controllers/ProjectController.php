8d068d49ed29fb2a8483837d3e72487cfd796315
stefanodisanza <87936828+stefanodisanza@users.noreply.github.com>
2023-04-20 15:42:21 +0200

    first commit

---
+++ app/Http/Controllers/ProjectController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreprojectRequest;
+use App\Http\Requests\UpdateprojectRequest;
+use App\Models\project;
+
+class ProjectController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \App\Http\Requests\StoreprojectRequest  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(StoreprojectRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function show(project $project)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(project $project)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \App\Http\Requests\UpdateprojectRequest  $request
+     * @param  \App\Models\project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function update(UpdateprojectRequest $request, project $project)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\project  $project
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(project $project)
+    {
+        //
+    }
+}
